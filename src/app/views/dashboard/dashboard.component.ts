d6ef0dc057f11b2eff4575dddb064300b9974b63
Mauricio Aguero <mam.121002@gmail.com>
2023-11-14 11:24:05 -0600

    perfect matches

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -13,7 +13,8 @@ export class DashboardComponent {
   // Atributes
   recipes: Recipe[] = [];
   recipesFiltered: Recipe[] = [];
-  searchFilter: Map<string, number> = new Map<string, number>();
+  searchFilter: string = ''
+  searchFilterMap: Map<string, number> = new Map<string, number>();
   currentPage = 1;
   recipesPerPage = 12;
   filterTimer: any;
@@ -33,50 +34,59 @@ export class DashboardComponent {
     return []
   }
 
+  // FILTER RECIPE
+  // Timer for Search Filter to update
+  resetFilterTimer(event: any) {
+    clearTimeout(this.filterTimer);
+    this.filterTimer = setTimeout(() => {
+      this.updateFilter(event);
+    }, 500); // 500ms = 0.5s
+    // }, 3000); // 3000ms = 3s
+  }
+
+  updateFilter(event: any) {
+    // remove previous filters
+    this.searchFilterMap.clear()
+
+    this.searchFilter = event.target.value.trim()
+    let words = this.getWords(this.searchFilter);
+    words.forEach((word: string) => {
+      this.addWordToMap(word, this.searchFilterMap)
+    });
+
+    // go to the first page after applying filter
+    this.goToPage(1)
+  }
+
   filterRecipes(): void {
-    let n = this.recipes.length
+      let n = this.recipes.length
     let filteredArray: Recipe[] = []
-    if (n > 1) {
+    if(n > 1) {
       // if theres a filter
-      if (this.searchFilter.size > 0) {
+      if (this.searchFilterMap.size > 0) {
         //arrays for sorting
+        let perfect_match_title: Recipe[] = []
+        let perfect_match_desc: Recipe[] = []
         let recipes_title_count: Recipe[] = []
         let recipes_description_count: Recipe[] = []
 
         let recipesArray: Recipe[] = this.recipes.slice().reverse()
         // for each recipe apply filter
         recipesArray.forEach(recipe => {
-          let recipeTitle = this.getWords(recipe.title.toLowerCase())
-          let recipeDescription = this.getWords(recipe.description.toLowerCase())
-
-          //variables for sorting
-          let inTitle = true
-          let inDescription = true
+          // check for perfect match
+          if (this.checkPerfectMatch(this.searchFilter, recipe.title)) {
+            perfect_match_title.push(recipe)
+          } else if (this.checkPerfectMatch(this.searchFilter, recipe.description)) {
+            perfect_match_desc.push(recipe)
+          }
 
           // check for every word of the filter in the recipe
-          this.searchFilter.forEach((count, word) => {
-            if (recipeTitle.includes(word)) {
-              inTitle = inTitle && true
-            } else{
-              inTitle = false
-              if (recipeDescription.includes(word)) {
-                inDescription = inDescription && true
-              } else{
-                inDescription = false
-              }
-            }
+          // this.searchFilterMap.forEach((count, word) => {
 
-            
-          });
-
-          if (inTitle) {
-            recipes_title_count.push(recipe)
-          }
-          else if (inDescription) {
-            recipes_description_count.push(recipe)
-          }
+          // });
         });
-        filteredArray = recipes_title_count.concat(recipes_description_count)
+        filteredArray = perfect_match_title
+        filteredArray = filteredArray.concat(perfect_match_desc)
       } else {
         // remove last recipe if no filters
         filteredArray = this.recipes.slice(0, -1)
@@ -88,32 +98,8 @@ export class DashboardComponent {
     this.recipesFiltered = filteredArray
   }
 
-  // Timer for Search Filter
-  resetFilterTimer(event: any) {
-    clearTimeout(this.filterTimer);
-    this.filterTimer = setTimeout(() => {
-      this.updateFilter(event);
-    }, 500); // 500ms = 0.5s
-    // }, 3000); // 3000ms = 3s
-  }
-
-  // Filter By Name
-  updateFilter(event: any) {
-    // remove previous filters
-    this.searchFilter.clear()
-
-    
-    let words = this.getWords(event.target.value);
-    words.forEach((word: string) => {
-      this.addWordToMap(word, this.searchFilter)
-    });
-
-    // go to the first page after applying filter
-    this.goToPage(1)
-  }
-
   // get all words with regex expression
-  getWords(str: string): string[]{
+  getWords(str: string): string[] {
     return str.split(/[\s\W]+/)
   }
 
@@ -128,6 +114,37 @@ export class DashboardComponent {
     }
   }
 
+  // check for all of the words in the string in the same order
+  checkPerfectMatch(words: string, str: string): boolean {
+    let word_array: string[] = this.getWords(words.toLowerCase())
+    let str_array: string[] = this.getWords(str.toLowerCase())
+
+    let index = -2 //-2 indicates that no word has been checked yet
+    word_array.forEach((word) => {
+      if (str_array.includes(word) && index != -1) {
+        if (index == -2) { //first word
+          index = str_array.indexOf(word)
+        } else {
+          if (index + 1 == str_array.indexOf(word)) {
+            index += 1
+          } else {
+            index = -1
+          }
+        }
+      } else {
+        index = -1
+      }
+    });
+
+    if (index > -1){
+      return true
+    }
+    return false
+  }
+
+
+
+  
   // Cards
   updatePageCardClicks(): void {
     // add click event to recipe cards
