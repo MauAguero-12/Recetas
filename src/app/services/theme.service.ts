59bf5e249ddbb2490ed5b681cdaede555a15e80b
Mauricio Aguero <mam.121002@gmail.com>
2023-11-17 11:15:15 -0600

    styles

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -23,16 +23,16 @@ export class ThemeService {
     const rootElement = document.documentElement
     switch (this.currentTheme) {
       case 'theme-default':
-        rootElement.style.setProperty('--pa-white', '#EBEBEB')
-        rootElement.style.setProperty('--pa-black', '#000000')
-        rootElement.style.setProperty('--pa-brown', '#774936')
-        rootElement.style.setProperty('--pa-shadow', '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)')
+        rootElement.style.setProperty('--pa-theme-white', '#EBEBEB')
+        rootElement.style.setProperty('--pa-theme-black', '#000000')
+        rootElement.style.setProperty('--pa-theme-brown', '#774936')
+        rootElement.style.setProperty('--pa-theme-shadow', '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)')
         break
       case 'theme-dark':
-        rootElement.style.setProperty('--pa-white', '#001000')
-        rootElement.style.setProperty('--pa-black', '#EBEBEB')
-        rootElement.style.setProperty('--pa-brown', '#D1B490')
-        rootElement.style.setProperty('--pa-shadow', '0 6px 12px 0 rgba(50, 147, 111, 0.2), 0 8px 20px 0 rgba(50, 147, 111, 0.19)')
+        rootElement.style.setProperty('--pa-theme-white', '#001000')
+        rootElement.style.setProperty('--pa-theme-black', '#EBEBEB')
+        rootElement.style.setProperty('--pa-theme-brown', '#D1B490')
+        rootElement.style.setProperty('--pa-theme-shadow', '0 6px 12px 0 rgba(50, 147, 111, 0.2), 2px -2px 1px 0 rgba(50, 147, 111, 0.2), 0 8px 20px 0 rgba(50, 147, 111, 0.19)')
         break
     }
     rootElement.style.setProperty('--pa-theme-transition', '0.5s')
